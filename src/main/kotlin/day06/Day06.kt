51fbe0d4755d23224700cb8a74ecff8b27e62688
Manu Casariego <manucasariego@Manus-Laptop.local>
2023-07-30 00:22:23 +0200

    Done day 6 and 7

--- src/main/kotlin/day06/Day06.kt
+++ src/main/kotlin/day06/Day06.kt
@@ -1,13 +1,126 @@
 package day06
 
 import Day
+import kotlin.math.max
 
 class Day06(private val input: String) : Day() {
+    data class Coordinate(val x: Int, val y: Int) {
+        companion object {
+            fun builder(line: String): Coordinate {
+                val split = line.split(",")
+                return Coordinate(split[0].toInt(), split[1].toInt())
+            }
+
+        }
+    }
+
+    data class Board(val board: Array<Array<Boolean>>) {
+        private fun turnOn(a: Coordinate, b: Coordinate) {
+            for (iterableX in a.x..b.x) for (iterableY in a.y..b.y) board[iterableX][iterableY] = true
+        }
+
+        private fun turnOff(a: Coordinate, b: Coordinate) {
+            for (iterableX in a.x..b.x) for (iterableY in a.y..b.y) board[iterableX][iterableY] = false
+        }
+
+        private fun toggle(a: Coordinate, b: Coordinate) {
+            // interact with board
+            for (iterableX in a.x..b.x) for (iterableY in a.y..b.y) board[iterableX][iterableY] =
+                !board[iterableX][iterableY]
+        }
+
+        fun execute(instruction: Instruction) {
+            when (instruction.operation) {
+                "turn on" -> this.turnOn(instruction.a, instruction.b)
+                "turn off" -> this.turnOff(instruction.a, instruction.b)
+                else -> this.toggle(instruction.a, instruction.b)
+            }
+        }
+
+        fun howManyLightsOn(): String {
+            var count = 0
+            for (array in board) {
+                for (boolean in array) {
+                    if (boolean) count++
+                }
+            }
+            return count.toString()
+        }
+    }
+
+    data class BoardPart2(val board: Array<Array<Int>>) {
+        private fun turnOn(a: Coordinate, b: Coordinate) {
+            for (iterableX in a.x..b.x) for (iterableY in a.y..b.y) board[iterableX][iterableY]++
+        }
+
+        private fun turnOff(a: Coordinate, b: Coordinate) {
+            for (iterableX in a.x..b.x) for (iterableY in a.y..b.y) board[iterableX][iterableY] =
+                max(0, board[iterableX][iterableY] - 1)
+        }
+
+        private fun toggle(a: Coordinate, b: Coordinate) {
+            // interact with board
+            for (iterableX in a.x..b.x) for (iterableY in a.y..b.y) board[iterableX][iterableY] += 2
+        }
+
+        fun execute(instruction: Instruction) {
+            when (instruction.operation) {
+                "turn on" -> this.turnOn(instruction.a, instruction.b)
+                "turn off" -> this.turnOff(instruction.a, instruction.b)
+                else -> this.toggle(instruction.a, instruction.b)
+            }
+        }
+
+        fun howManyLightsOn(): String {
+            var count = 0
+            for (array in board) {
+                for (int in array) {
+                    count += int
+                }
+            }
+            return count.toString()
+        }
+    }
+
+    data class Instruction(val operation: String, val a: Coordinate, val b: Coordinate) {
+        companion object {
+            fun builder(line: String): Instruction {
+                val split = line.split(" ")
+                return if (line.contains("turn on")) {
+                    // turn on operation
+                    Instruction("turn on", Coordinate.builder(split[2]), Coordinate.builder(split[4]))
+                } else if (line.contains("turn off")) {
+                    // turn off operation
+                    Instruction("turn off", Coordinate.builder(split[2]), Coordinate.builder(split[4]))
+                } else {
+                    // toggle operation
+                    Instruction("toggle", Coordinate.builder(split[1]), Coordinate.builder(split[3]))
+                }
+            }
+        }
+
+    }
+
     override fun part1(): String {
-        TODO("Not yet implemented")
+        val (sizeX, sizeY) = listOf(1000, 1000)
+        val board = Board(Array(sizeX) { Array(sizeY) { false } })
+        input.lines().map {
+            Instruction.builder(it)
+        }.forEach {
+            board.execute(it)
+        }
+        return board.howManyLightsOn()
+
     }
 
     override fun part2(): String {
-        TODO("Not yet implemented")
+        val (sizeX, sizeY) = listOf(1000, 1000)
+        val board = BoardPart2(Array(sizeX) { Array(sizeY) { 0 } })
+        input.lines().map {
+            Instruction.builder(it)
+        }.forEach {
+            board.execute(it)
+        }
+        return board.howManyLightsOn()
     }
 }
--- src/main/kotlin/day07/Day07.kt
+++ src/main/kotlin/day07/Day07.kt
@@ -1,13 +1,189 @@
 package day07
 
 import Day
+import java.util.*
+import kotlin.collections.Map
+import kotlin.math.abs
+import kotlin.math.pow
 
 class Day07(private val input: String) : Day() {
+    data class Signal(val array: Array<Boolean>) {
+        companion object {
+            fun builder(): Signal {
+                return Signal(Array(16) { false })
+            }
+
+            fun builder(int: Int): Signal {
+                return Signal(Array(16) { i -> ((int shr (15 - i)) and 1) == 1 })
+            }
+        }
+
+        fun copy(): Signal {
+            return Signal(array)
+        }
+
+        fun and(other: Signal): Signal {
+            val result = builder()
+            for (i in array.indices) {
+                result.array[i] = this.array[i] and other.array[i]
+            }
+            return result
+        }
+
+        fun or(other: Signal): Signal {
+            val result = builder()
+            for (i in array.indices) {
+                result.array[i] = this.array[i] or other.array[i]
+            }
+            return result
+        }
+
+        fun not(): Signal {
+            val result = builder()
+            for (i in array.indices) {
+                result.array[i] = !this.array[i]
+            }
+            return result
+        }
+
+        private fun rShift(): Signal {
+            val result = builder()
+
+            for (i in 1..<array.size) {
+                result.array[i] = this.array[i - 1]
+            }
+            return result
+        }
+
+        fun rShift(int: Int): Signal {
+            var intermediate = this
+            for (i in 0..<int) intermediate = intermediate.rShift()
+            return intermediate
+        }
+
+        private fun lShift(): Signal {
+            val result = builder()
+
+            for (i in 0..<array.size - 1) {
+                result.array[i] = this.array[i + 1]
+            }
+            return result
+        }
+
+        fun lShift(int: Int): Signal {
+            var intermediate = this
+            for (i in 0..<int) intermediate = intermediate.lShift()
+            return intermediate
+        }
+
+        fun toInt(): Int {
+            var count = 0
+            for (i in array.indices) {
+                if (array[i]) {
+                    val power = abs(i - (array.size - 1))
+                    count += 2.0.pow(power.toDouble()).toInt()
+                }
+            }
+            return count
+        }
+    }
+
+
     override fun part1(): String {
-        TODO("Not yet implemented")
+        var signalMap = mutableMapOf<String, Signal>()
+        val instructionMap = mutableMapOf<String, String>()
+        input.lines().forEach {
+            val split = it.split(" ")
+            instructionMap[split.last()] = it
+        }
+        populateMapForSignal(signalMap, "a", instructionMap)
+        return signalMap["a"]!!.toInt().toString()
+    }
+
+
+    private fun populateMapForSignal(
+        signalMap: MutableMap<String, Signal>,
+        signal: String,
+        instructionMap: Map<String, String>
+    ) {
+        val instructionStack = Stack<String>()
+        instructionStack.push(instructionMap[signal])
+        while (instructionStack.isNotEmpty()) {
+            val instruction = instructionStack.peek()
+            val splitInstruction = instruction.split(" ")
+            // evaluate
+            if (instruction.contains("OR") || instruction.contains("AND")) {
+                val a = getSignal(signalMap, splitInstruction[0])
+                val b = getSignal(signalMap, splitInstruction[2])
+                if (a == null) {
+                    instructionStack.push(instructionMap[splitInstruction[0]])
+                }
+                if (b == null) {
+                    instructionStack.push(instructionMap[splitInstruction[2]])
+                }
+                if (a != null && b != null) {
+                    if (instruction.contains("OR")) signalMap[splitInstruction[4]] = a.or(b)
+                    else signalMap[splitInstruction[4]] = a.and(b)
+                    instructionStack.pop()
+                }
+            } else if (instruction.contains("NOT")) {
+                val a = getSignal(signalMap, splitInstruction[1])
+                if (a == null) {
+                    instructionStack.push(instructionMap[splitInstruction[1]])
+                }
+                if (a != null) {
+                    signalMap[splitInstruction[3]] = a.not()
+                    instructionStack.pop()
+                }
+            } else if (instruction.contains("SHIFT")) {
+                val a = getSignal(signalMap, splitInstruction[0])
+                if (a == null) {
+                    instructionStack.push(instructionMap[splitInstruction[0]])
+                }
+                if (a != null) {
+                    if (instruction.contains("LSHIFT")) signalMap[splitInstruction[4]] =
+                        a.lShift(splitInstruction[2].toInt())
+                    else signalMap[splitInstruction[4]] = a.rShift(splitInstruction[2].toInt())
+                    instructionStack.pop()
+                }
+            } else {
+                // set operation
+                val a = getSignal(signalMap, splitInstruction[0])
+                if (a == null) {
+                    instructionStack.push(instructionMap[splitInstruction[0]])
+                }
+                if (a != null) {
+                    signalMap[splitInstruction[2]] = a.copy()
+                    instructionStack.pop()
+                }
+            }
+        }
+    }
+
+    private fun getSignal(map: Map<String, Signal>, string: String): Signal? {
+        // three options:
+        // A. it's already on the map
+        if (map.containsKey(string)) return map[string]!!
+        // B. it's a numeric value
+        if (string[0].isDigit()) return Signal.builder(string.toInt())
+        // C. the instruction should be blocked as we don't have this value yet
+        return null
     }
 
     override fun part2(): String {
-        TODO("Not yet implemented")
+        var signalMap = mutableMapOf<String, Signal>()
+        val instructionMap = mutableMapOf<String, String>()
+        input.lines().forEach {
+            val split = it.split(" ")
+            instructionMap[split.last()] = it
+        }
+        populateMapForSignal(signalMap, "a", instructionMap)
+        val firstValueOfA = signalMap["a"]!!.toInt()
+        // resetting the map
+        signalMap = mutableMapOf<String, Signal>()
+        // setting to b the old value of a
+        signalMap["b"] = Signal.builder(firstValueOfA)
+        populateMapForSignal(signalMap, "a", instructionMap)
+        return signalMap["a"]!!.toInt().toString()
     }
 }
