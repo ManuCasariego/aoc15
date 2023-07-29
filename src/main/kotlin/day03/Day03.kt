1afd91bae03e4dc16de88877e043b43d3eb37ccf
Manu Casariego <manucasariego@Manus-Laptop.local>
2023-07-29 09:54:07 +0200

    Done day 3

--- src/main/kotlin/day03/Day03.kt
+++ src/main/kotlin/day03/Day03.kt
@@ -3,11 +3,43 @@ package day03
 import Day
 
 class Day03(private val input: String) : Day() {
+
+    data class Point(val x: Int, val y: Int)
+
     override fun part1(): String {
-        TODO("Not yet implemented")
+        val houses = mutableSetOf<Point>()
+        var (currentX, currentY) = listOf(0, 0)
+        houses.add(Point(currentX, currentY))
+        input.forEach { c: Char ->
+            when (c) {
+                '>' -> currentX += 1
+                '<' -> currentX -= 1
+                'v' -> currentY -= 1
+                '^' -> currentY += 1
+            }
+            houses.add(Point(currentX, currentY))
+        }
+        return houses.size.toString()
     }
 
     override fun part2(): String {
-        TODO("Not yet implemented")
+        val houses = mutableSetOf<Point>()
+        var (currentSantaX, currentSantaY) = listOf(0, 0)
+        var (currentDoggyX, currentDoggyY) = listOf(0, 0)
+        houses.add(Point(currentSantaX, currentSantaY))
+        var santaTurn = true
+        input.forEach { c: Char ->
+            if (c == '>') if (santaTurn) currentSantaX++ else currentDoggyX++
+            else if (c == '<') if (santaTurn) currentSantaX-- else currentDoggyX--
+            else if (c == 'v') if (santaTurn) currentSantaY-- else currentDoggyY--
+            else if (c == '^') if (santaTurn) currentSantaY++ else currentDoggyY++
+            if (santaTurn) houses.add(Point(currentSantaX, currentSantaY)) else houses.add(
+                Point(
+                    currentDoggyX, currentDoggyY
+                )
+            )
+            santaTurn = !santaTurn
+        }
+        return houses.size.toString()
     }
 }
