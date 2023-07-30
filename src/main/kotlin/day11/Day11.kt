ca7fe4284eba28d8ce6f314c452af131983f2efb
manuel.casariego <manucasariego@gmail.com>
2023-07-30 22:13:31 +0200

    Done day 11

--- src/main/kotlin/day11/Day11.kt
+++ src/main/kotlin/day11/Day11.kt
@@ -7,7 +7,116 @@ class Day11(private val input: String) : Day() {
         TODO("Not yet implemented")
     }
 
+    fun part1String(): String {
+        val password = Password.builder(input)
+        while (!validPassword(password)) {
+            password.add()
+        }
+        return password.toString()
+    }
+
+    private fun validPassword(password: Password): Boolean {
+        //Passwords must include one increasing straight of at least three letters, like abc, bcd, cde, and so on, up to xyz. They cannot skip letters; abd doesn't count.
+        var condition = false
+        for (i in 0..5) {
+            if (password.password[i] + 1 == password.password[i + 1] && password.password[i] + 2 == password.password[i + 2]
+            ) condition = true
+        }
+        if (!condition) return false
+
+        //Passwords may not contain the letters i, o, or l, as these letters can be mistaken for other characters and are therefore confusing.
+        if (password.password.contains(Password.alphabet.indexOf('i')) ||
+            password.password.contains(Password.alphabet.indexOf('o')) ||
+            password.password.contains(Password.alphabet.indexOf('l'))
+        ) return false
+
+        //Passwords must contain at least two different, non-overlapping pairs of letters, like aa, bb, or zz.
+        var pairOfLeters = mutableSetOf<Char>()
+        for (i in 0..6) {
+            if (password.password[i] == password.password[i + 1]) pairOfLeters.add(
+                Password.alphabet.elementAt(
+                    password.password[i]
+                )
+            )
+        }
+        return (pairOfLeters.size >= 2)
+    }
+
+    data class Password(val password: Array<Int>) {
+        override fun toString(): String {
+            val stringBuilder = StringBuilder()
+            for (pwdChar in password) {
+                stringBuilder.append(alphabet[pwdChar])
+            }
+            return stringBuilder.toString()
+        }
+
+        fun add() {
+            for (i in 7 downTo 0) {
+                if (password[i] + 1 < alphabet.size) {
+                    password[i]++
+                    return
+                } else {
+                    password[i] = 0
+                }
+            }
+            print("we got to the end - 00000000")
+        }
+
+        companion object {
+            val alphabet = listOf(
+                'a',
+                'b',
+                'c',
+                'd',
+                'e',
+                'f',
+                'g',
+                'h',
+                'i',
+                'j',
+                'k',
+                'l',
+                'm',
+                'n',
+                'o',
+                'p',
+                'q',
+                'r',
+                's',
+                't',
+                'u',
+                'v',
+                'w',
+                'x',
+                'y',
+                'z'
+            )
+
+            fun builder(string: String): Password {
+                val array = Array(8) { 0 }
+                for (i in 0..7) {
+                    array[i] = alphabet.indexOf(string[i])
+                }
+                return Password(array)
+            }
+        }
+    }
+
     override fun part2(): Int {
         TODO("Not yet implemented")
+    }
+
+
+    fun part2String(): String {
+        val password = Password.builder(input)
+        while (!validPassword(password)) {
+            password.add()
+        }
+        password.add()
+        while (!validPassword(password)) {
+            password.add()
+        }
+        return password.toString()
     }
 }
--- src/test/kotlin/day11/Day11Test.kt
+++ src/test/kotlin/day11/Day11Test.kt
@@ -3,6 +3,7 @@ package day11
 import DayTest
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
 
 
 class Day11Test : DayTest() {
@@ -11,26 +12,30 @@ class Day11Test : DayTest() {
 
     @Test
     fun testSolvePart1WithExampleInput() {
-        val part1 = solvePart1(exampleDay)
-        val expectedSolution = -1
+        val part1 = exampleDay.part1String()
+        val expectedSolution = "abcdffaa"
         Assertions.assertEquals(expectedSolution, part1, "Part 1 with example input should equal $expectedSolution")
     }
 
     @Test
     fun testSolvePart2WithExampleInput() {
-        val part2 = solvePart2(exampleDay)
-        val expectedSolution = -1
+        val part2 = exampleDay.part2String()
+        val expectedSolution = "abcdffbb"
         Assertions.assertEquals(expectedSolution, part2, "Part 2 with example input should equal $expectedSolution")
     }
 
     @Test
     fun part1() {
-        solvePart1(day)
+        val part1 = day.part1String()
+        println("the solution to part 1 is $part1")
+        assertEquals("vzbxxyzz", part1)
     }
 
     @Test
     fun part2() {
-        solvePart2(day)
+        val part2 = day.part2String()
+        println("the solution to part 2 is $part2")
+        assertEquals("vzcaabcc", part2)
     }
 
 }
