d49f1c3351a53c6c7fef9b8663db48d69957a61b
manuel.casariego <manucasariego@gmail.com>
2023-07-30 16:47:56 +0200

    Done day 8

--- src/main/kotlin/day08/Day08.kt
+++ src/main/kotlin/day08/Day08.kt
@@ -4,10 +4,25 @@ import Day
 
 class Day08(private val input: String) : Day() {
     override fun part1(): String {
-        TODO("Not yet implemented")
+        var totalChars = 0
+        var totalEscapedChars = 0
+        input.lines().forEach {
+            totalChars += it.length
+            var escapedString = it.subSequence(1, it.length - 1).replace(regex = Regex("\\\\\\\""), replacement =  "\"").replace(regex = "\\\\\\\\".toRegex(), replacement =  "\\\\")
+            escapedString = escapedString.replace("\\\\x([0-9|a-f]){2}".toRegex(), "n")
+            totalEscapedChars += escapedString.length
+        }
+        return (totalChars - totalEscapedChars).toString()
     }
 
     override fun part2(): String {
-        TODO("Not yet implemented")
+        var totalChars = 0
+        var totalEscapedChars = 0
+        input.lines().forEach {
+            totalChars += it.length
+            val escapedString = it.replace(regex = Regex("\\\\"), replacement =  "oo").replace(regex = "\\\"".toRegex(), replacement =  "oo")
+            totalEscapedChars += escapedString.length + 2
+        }
+        return (totalEscapedChars - totalChars).toString()
     }
 }
