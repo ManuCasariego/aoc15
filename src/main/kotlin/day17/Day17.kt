e319a1b2fd8afea4586c2bf0673a665a2d262ee5
manuel.casariego <manucasariego@gmail.com>
2023-07-31 21:21:06 +0200

    Done day 17

--- src/main/kotlin/day17/Day17.kt
+++ src/main/kotlin/day17/Day17.kt
@@ -4,10 +4,54 @@ import Day
 
 class Day17(private val input: String) : Day() {
     override fun part1(): Int {
-        TODO("Not yet implemented")
+        stateMap = mutableMapOf()
+        usedContainers = mutableSetOf()
+
+        var i = 0
+        val containers = input.lines().map { Container(i++, it.toInt()) }.toSet()
+        magic(150, containers)
+        return usedContainers.size
+
     }
 
+    data class State(val goal: Int, val availableContainers: Set<Container>)
+
+    var stateMap = mutableMapOf<State, Int>()
+    var usedContainers = mutableSetOf<Set<Container>>()
+    private fun magic(goal: Int, availableContainers: Set<Container>): Int {
+        if (goal <= 0) return 0
+        // choose every container and go down one level
+        val state = State(goal, availableContainers)
+        if (stateMap.contains(state)) return stateMap[state]!!
+        // from any given state, you want to check how many available containers combinations can be fit
+        var solution = 0
+        availableContainers.filter { it.quantity <= goal }.forEach {
+            if (it.quantity == goal) {
+                if (usedContainers.contains(availableContainers.minus(it))) {
+
+                } else {
+                    solution++
+                    usedContainers.add(availableContainers.minus(it))
+                }
+            }
+            // check if we already got that from some other path, if so, don't add 1
+            else solution += magic(goal - it.quantity, availableContainers.minus(it))
+        }
+        stateMap[state] = solution
+        return solution
+    }
+
+    data class Container(val index: Int, val quantity: Int)
+
     override fun part2(): Int {
-        TODO("Not yet implemented")
+        stateMap = mutableMapOf()
+        usedContainers = mutableSetOf()
+
+        var i = 0
+        val containers = input.lines().map { Container(i++, it.toInt()) }.toSet()
+        magic(150, containers)
+
+        val leastAmountOfContainersUsed = usedContainers.maxOf { it.size }
+        return usedContainers.count { it.size == leastAmountOfContainersUsed }
     }
 }
--- src/test/kotlin/day17/Day17Test.kt
+++ src/test/kotlin/day17/Day17Test.kt
@@ -1,7 +1,6 @@
 package day17
 
 import DayTest
-import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
 
@@ -9,19 +8,6 @@ class Day17Test : DayTest() {
     private val exampleDay = Day17("17".readTestInput())
     private val day = Day17("17".readInput())
 
-    @Test
-    fun testSolvePart1WithExampleInput() {
-        val part1 = solvePart1(exampleDay)
-        val expectedSolution = -1
-        Assertions.assertEquals(expectedSolution, part1, "Part 1 with example input should equal $expectedSolution")
-    }
-
-    @Test
-    fun testSolvePart2WithExampleInput() {
-        val part2 = solvePart2(exampleDay)
-        val expectedSolution = -1
-        Assertions.assertEquals(expectedSolution, part2, "Part 2 with example input should equal $expectedSolution")
-    }
 
     @Test
     fun part1() {
