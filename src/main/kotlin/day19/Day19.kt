7b85d0fba77d47167169cc872b14b203052738ec
Manuel Casariego <manuel.casariego@klarna.com>
2023-08-02 10:52:25 +0200

    done day 19

--- src/main/kotlin/day19/Day19.kt
+++ src/main/kotlin/day19/Day19.kt
@@ -1,13 +1,58 @@
 package day19
 
 import Day
+import java.util.Stack
 
 class Day19(private val input: String) : Day() {
-    override fun part1(): Int {
-        TODO("Not yet implemented")
-    }
+  override fun part1(): Int {
+    val replacements = input.split("\n\n")[0].lines().map { Replacement.parse(it) }
+    val molecule = input.split("\n\n")[1]
 
-    override fun part2(): Int {
-        TODO("Not yet implemented")
+    return replacements.map { applyReplacement(it, molecule) }.flatten().toSet().size
+  }
+
+  override fun part2(): Int {
+    val reverseReplacements = input.split("\n\n")[0].lines().map { Replacement.parseReverse(it) }
+    val molecule = input.split("\n\n")[1]
+
+    val levels = mutableListOf<Int>()
+
+    // let's reverse it, start from the molecule, and work down towards "e"
+    // using a queue, so I can prioritise going down in size
+    val stack = Stack<String>()
+    stack.add(molecule)
+    val numberOfTransformationsMap = mutableMapOf(molecule to 0)
+    while (stack.isNotEmpty() && !stack.contains("e")) {
+      val current = stack.pop()
+      val shortestTwo =
+        reverseReplacements.map { applyReplacement(it, current) }.flatten().toSet().sortedBy { it.length }.take(2).filter {!numberOfTransformationsMap.contains(it)}
+          .toList()
+      stack.addAll(shortestTwo)
+      levels.add(numberOfTransformationsMap[current]!!)
+      shortestTwo.forEach { numberOfTransformationsMap.putIfAbsent(it, numberOfTransformationsMap[current]!! + 1)}
     }
+    println(levels)
+    return numberOfTransformationsMap.getOrDefault("e", -1)
+  }
+
+  private fun applyReplacement(replacement: Replacement, molecule: String): Set<String> {
+    return replacement.from.findAll(molecule).map { molecule.replaceRange(it.range, replacement.to) }.toSet()
+  }
+
+
+  data class Replacement(val from: Regex, val to: String) {
+    companion object {
+      fun parse(line: String): Replacement {
+        // Ca => PRnFAr
+        val (from, to) = """(\w+) => (\w+)""".toRegex().find(line)!!.destructured
+        return Replacement(from.toRegex(), to)
+      }
+
+      fun parseReverse(line: String): Replacement {
+        // Ca => PRnFAr
+        val (to, from) = """(\w+) => (\w+)""".toRegex().find(line)!!.destructured
+        return Replacement(from.toRegex(), to)
+      }
+    }
+  }
 }
--- src/test/kotlin/day19/Day19Test.kt
+++ src/test/kotlin/day19/Day19Test.kt
@@ -6,31 +6,32 @@ import org.junit.jupiter.api.Test
 
 
 class Day19Test : DayTest() {
-    private val exampleDay = Day19("19".readTestInput())
-    private val day = Day19("19".readInput())
+  private val exampleDay = Day19("19".readTestInput())
+  private val day = Day19("19".readInput())
 
-    @Test
-    fun testSolvePart1WithExampleInput() {
-        val part1 = solvePart1(exampleDay)
-        val expectedSolution = -1
-        Assertions.assertEquals(expectedSolution, part1, "Part 1 with example input should equal $expectedSolution")
-    }
+  @Test
+  fun testSolvePart1WithExampleInput() {
+    val part1 = solvePart1(exampleDay)
+    val expectedSolution = 7
+    Assertions.assertEquals(expectedSolution, part1, "Part 1 with example input should equal $expectedSolution")
+  }
 
-    @Test
-    fun testSolvePart2WithExampleInput() {
-        val part2 = solvePart2(exampleDay)
-        val expectedSolution = -1
-        Assertions.assertEquals(expectedSolution, part2, "Part 2 with example input should equal $expectedSolution")
-    }
+  @Test
+  fun testSolvePart2WithExampleInput() {
+    val part2 = solvePart2(exampleDay)
+    val expectedSolution = 6
+    Assertions.assertEquals(expectedSolution, part2, "Part 2 with example input should equal $expectedSolution")
+  }
 
-    @Test
-    fun part1() {
-        solvePart1(day)
-    }
+  @Test
+  fun part1() {
+    solvePart1(day)
+  }
 
-    @Test
-    fun part2() {
-        solvePart2(day)
-    }
+  @Test
+  fun part2() {
+    solvePart2(day)
+  }
+
 
 }
