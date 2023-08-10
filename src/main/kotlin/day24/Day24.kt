e4228416cad9b11a2b20038ce5a58ac2b8861a25
manuel.casariego <manucasariego@gmail.com>
2023-08-10 23:21:44 +0200

    first commit for day 24

--- src/main/kotlin/day24/Day24.kt
+++ src/main/kotlin/day24/Day24.kt
@@ -4,7 +4,12 @@ import Day
 
 class Day24(private val input: String) : Day() {
     override fun part1(): Int {
-        TODO("Not yet implemented")
+        val totalWeight = input.lines().sumOf { it.toInt() }
+        // each group needs to weight totalWeight / 3
+        for (i in 1..input.lines().size / 3) {
+
+        }
+
     }
 
     override fun part2(): Int {
