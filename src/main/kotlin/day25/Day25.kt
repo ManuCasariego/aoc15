f51faa71c467dd5a806c9d0cae8cec86d0565c38
manuel.casariego <manucasariego@gmail.com>
2023-08-11 19:26:56 +0200

    done day 25

--- src/main/kotlin/day25/Day25.kt
+++ src/main/kotlin/day25/Day25.kt
@@ -3,11 +3,56 @@ package day25
 import Day
 
 class Day25(private val input: String) : Day() {
-    override fun part1(): Int {
-        TODO("Not yet implemented")
+    fun part1Long(): Long {
+        val regex =
+            """To continue, please consult the code grid in the manual. {2}Enter the code at row (\d+), column (\d+).""".toRegex()
+
+        val (rowStr, columnStr) = regex.find(input)!!.destructured
+        val row = rowStr.toInt()
+        val column = columnStr.toInt()
+
+        // we need to turn the combination of row, number into an actual index
+        // notation row, column
+        //    | 1   2   3   4   5   6
+        //---+---+---+---+---+---+---+
+        // 1 |  1   3   6  10  15  21
+        // 2 |  2   5   9  14  20
+        // 3 |  4   8  13  19
+        // 4 |  7  12  18
+        // 5 | 11  17
+        // 6 | 16
+
+        // 00 -> 1, 1
+        // 01 -> 2, 1
+        // 02 -> 1, 2
+        // 03 -> 3, 1
+        // 04 -> 2, 2
+        // 05 -> 1, 3
+        // 06 -> 4, 1
+        // 07 -> 3, 2
+        // 08 -> 2, 3
+        // 09 -> 1, 4
+        // 10 -> 5, 1
+
+        val index = getIndex(row, column)
+        return (1..index).fold(20151125L) { agg, _ -> nextPwd(agg) }
     }
 
+    private fun getIndex(row: Int, column: Int): Int {
+        val loop = row + (column - 1)
+        return (1..<loop).sum() + (column - 1)
+    }
+
+    private fun nextPwd(prev: Long): Long {
+        return prev * 252533 % 33554393
+    }
+
+    override fun part1(): Int {
+        TODO("No need to implement")
+    }
+
+
     override fun part2(): Int {
-        TODO("Not yet implemented")
+        TODO("No need to implement")
     }
 }
--- src/test/kotlin/day25/Day25Test.kt
+++ src/test/kotlin/day25/Day25Test.kt
@@ -11,26 +11,15 @@ class Day25Test : DayTest() {
 
     @Test
     fun testSolvePart1WithExampleInput() {
-        val part1 = solvePart1(exampleDay)
-        val expectedSolution = -1
+        val part1 = exampleDay.part1Long()
+        val expectedSolution = 27995004L
         Assertions.assertEquals(expectedSolution, part1, "Part 1 with example input should equal $expectedSolution")
     }
 
     @Test
-    fun testSolvePart2WithExampleInput() {
-        val part2 = solvePart2(exampleDay)
-        val expectedSolution = -1
-        Assertions.assertEquals(expectedSolution, part2, "Part 2 with example input should equal $expectedSolution")
-    }
-
-    @Test
     fun part1() {
-        solvePart1(day)
+        val part1 = day.part1Long()
+        val expectedSolution = 8997277L
+        Assertions.assertEquals(expectedSolution, part1, "Part 1 with example input should equal $expectedSolution")
     }
-
-    @Test
-    fun part2() {
-        solvePart2(day)
-    }
-
 }
